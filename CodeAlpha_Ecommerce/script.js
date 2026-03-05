fa3d858de20e20b5f60f14ec8778d3079151f47b
HemantKarache <karchehemant123@gmail.com>
2026-03-05 23:06:01 +0530

    Add files via upload

---
+++ CodeAlpha_Ecommerce/script.js
@@ -0,0 +1,35 @@
+function addToCart(name,price){
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    
+    cart.push({name,price});
+    
+    localStorage.setItem("cart",JSON.stringify(cart));
+    
+    alert("Product Added To Cart");
+    
+    }
+    
+    if(document.getElementById("cartItems")){
+    
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    
+    let list = document.getElementById("cartItems");
+    
+    let total = 0;
+    
+    cart.forEach(item=>{
+    
+    let li = document.createElement("li");
+    
+    li.textContent = item.name + " - ₹" + item.price;
+    
+    list.appendChild(li);
+    
+    total += item.price;
+    
+    });
+    
+    document.getElementById("total").textContent="Total: ₹"+total;
+    
+    }
